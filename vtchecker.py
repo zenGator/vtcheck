0f6e691947426a3e746dd67c03a7830ec112ed98
zG <22377978+zenGator@users.noreply.github.com>
2019-01-03 16:15:57 -0500

    Update vtchecker.py

--- vtchecker.py
+++ vtchecker.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # python3
 # VT_API_KEY set in .bashrc:  export VT_API_KEY='blahblahblah'
 # 
@@ -7,9 +8,9 @@ import requests
 import json
 import sys
 
-print "This is the name of the script: ", sys.argv[0]
-print "Number of arguments: ", len(sys.argv)
-print "The arguments are: " , str(sys.argv)
+#print ("This is the name of the script: ", sys.argv[0])
+print ("Processing %d hashes." % len(sys.argv))
+#print ("The arguments are: " , str(sys.argv))
 
 #see https://www.tutorialspoint.com/python/python_command_line_arguments.htm
 
@@ -19,9 +20,15 @@ headers = {
   "User-Agent" : "zGvtChecker"
   }
 params = {'apikey': os.environ['VT_API_KEY'], 'resource':'[md5hash]'}
-response = requests.get('https://www.virustotal.com/vtapi/v2/file/report',  params=params, headers=headers)
-json_response = response.json()
-#show with:  json_response
+
+for h in sys.argv[1:]:
+       print("checking on %s . . . " % h)
+       params = {'apikey': os.environ['VT_API_KEY'], 'resource':h}
+       response = requests.get('https://www.virustotal.com/vtapi/v2/file/report',  params=params, headers=headers)
+       json_response = response.json()
+#       print (json_response)
+       print("\tsha is %s" % json_response['sha1'])
+       print(json_response)
 
 # Access individual elements with: json_response['sha1']
 
