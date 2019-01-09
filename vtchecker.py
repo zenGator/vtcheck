23d9c4942395bd47a1b30d991169cb0f92d690d3
zG <22377978+zenGator@users.noreply.github.com>
2019-01-09 15:36:19 -0500

    Update vtchecker.py

--- vtchecker.py
+++ vtchecker.py
@@ -7,6 +7,8 @@ import os
 import requests
 import json
 import sys
+import re
+import time
 
 #print ("This is the name of the script: ", sys.argv[0])
 print ("Processing %d hashes." % (len(sys.argv)-1))
@@ -14,38 +16,50 @@ print ("Processing %d hashes." % (len(sys.argv)-1))
 
 #see https://www.tutorialspoint.com/python/python_command_line_arguments.htm
 
-
+posHash={}
+negHash={}
 headers = {
   "Accept-Encoding": "gzip, deflate",
   "User-Agent" : "zGvtChecker"
   }
-params = {'apikey': os.environ['VT_API_KEY'], 'resource':'[md5hash]'}
+#params = {'apikey': os.environ['VT_API_KEY'], 'resource':'[md5hash]'}
 
 for h in sys.argv[1:]:
-#ToDo:
-    '''ToDo:  test that each "hash" is in fact [0-9a-z]{32} 
-    '''
-    print("checking on %s . . . " % h)
-    params = {'apikey': os.environ['VT_API_KEY'], 'resource':h}
-    response = requests.get('https://www.virustotal.com/vtapi/v2/file/report',  params=params, headers=headers)
-    json_response = response.json()
-#       print (json_response)
-#       print("\tsha is %s" % json_response['sha1'])
-#ToDo:
-    '''ToDo: test whether any response came back.  A really bad request will engender a simple "[]"; otherwise we should get a json object with, at a minimum:      
-        response_code
-        resource
-        verbose_msg
-    '''
-    myPpty='response_code'
-    myResponse=json_response[myPpty]
-    print ("\t%s is %s" % (myPpty, myResponse) )
-    if myResponse==0 :
-        print ('no record')
-        # we have either submitted a bad request (but this should have been filtered with the test for a valid has at the beginning of the for loop) or there is no record (verbose_msg =~ "The requested resource is not among the finished, queued or pending scans"
+#    print (re.search(r"[0-9a-f]{32}", h))
+    if re.search(r"\b[0-9a-f]{32}\b", h, re.IGNORECASE):  #ensure input is a valid hash value
+        print("checking on %s . . . " % h)
+        params = {'apikey': os.environ['VT_API_KEY'], 'resource':h}
+        response = requests.get('https://www.virustotal.com/vtapi/v2/file/report',  params=params, headers=headers)
+        while response.status_code==204:
+#ToDo:  add countdown timer here; 1-minute timeout
+            print ("Rate limit.  Pausing a moment.  Please be patient . . . . \r", end='')
+            time.sleep(30)
+            response = requests.get('https://www.virustotal.com/vtapi/v2/file/report',  params=params, headers=headers)
+            
+#        print (response)
+        json_response = response.json()
+    #       print("\tsha is %s" % json_response['sha1'])
+    #ToDo:
+        '''ToDo: test whether any response came back.  A really bad request will engender a simple "[]"; otherwise we should get a json object with, at a minimum:      
+            response_code
+            resource
+            verbose_msg
+        '''
+        myPpty='response_code'
+        myResponse=json_response[myPpty]
+#        print ("\t%s is %s" % (myPpty, myResponse) )
+        if myResponse==0 :
+            print ('no record')
+            # we have either submitted a bad request (but this should have been filtered with the test for a valid has at the beginning of the for loop) or there is no record (verbose_msg =~ "The requested resource is not among the finished, queued or pending scans"
+        else:
+            print ('%d of %d scans identified this as malicious' % (json_response['positives'], json_response['total']))
+            if json_response['positives']:
+                posHash[h]=json_response['positives']
+            else:
+                negHash[h]=0
     else:
-         print ('found')   
-       
+            print ('>%s< is not a valid MD5 hash' % h)
+#ToDo:  print summary:  posHash, negHash
 print ('done')
 
 # Access individual elements with: json_response['sha1']
