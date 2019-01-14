851020fffb33289d86c6b953aac3eab11772a64d
zG <22377978+zenGator@users.noreply.github.com>
2019-01-14 08:03:47 -0500

    Update vtchecker.py

--- vtchecker.py
+++ vtchecker.py
@@ -18,21 +18,30 @@ print ("Processing %d hashes." % (len(sys.argv)-1))
 
 posHash={}
 negHash={}
+posObjects={}
+negObjects={}
+unkObjects=[]
 headers = {
   "Accept-Encoding": "gzip, deflate",
   "User-Agent" : "zGvtChecker"
   }
+c=0
 #params = {'apikey': os.environ['VT_API_KEY'], 'resource':'[md5hash]'}
 
 for h in sys.argv[1:]:
+    c+=1
 #    print (re.search(r"[0-9a-f]{32}", h))
     if re.search(r"\b[0-9a-f]{32}\b", h, re.IGNORECASE):  #ensure input is a valid hash value
         print("checking on %s . . . " % h)
         params = {'apikey': os.environ['VT_API_KEY'], 'resource':h}
         response = requests.get('https://www.virustotal.com/vtapi/v2/file/report',  params=params, headers=headers)
+        if c > 4:
+            print ("Rate limit.  Pausing for 16 seconds.  Please be patient . . . . \r", end='')
+            time.sleep(16)
+            response = requests.get('https://www.virustotal.com/vtapi/v2/file/report',  params=params, headers=headers)
         while response.status_code==204:
 #ToDo:  add countdown timer here; 1-minute timeout
-            print ("Rate limit.  Pausing a moment.  Please be patient . . . . \r", end='')
+            print ("Rate limit.  Pausing a bit longer.  Please be patient . . . . \r", end='')
             time.sleep(30)
             response = requests.get('https://www.virustotal.com/vtapi/v2/file/report',  params=params, headers=headers)
             
@@ -44,23 +53,32 @@ for h in sys.argv[1:]:
             response_code
             resource
             verbose_msg
+            
+            alternatively, test response.status_code
         '''
         myPpty='response_code'
         myResponse=json_response[myPpty]
+        print ('%s\r' % (" " * 80), end='')
 #        print ("\t%s is %s" % (myPpty, myResponse) )
         if myResponse==0 :
-            print ('no record')
+            print ('No record')
             # we have either submitted a bad request (but this should have been filtered with the test for a valid has at the beginning of the for loop) or there is no record (verbose_msg =~ "The requested resource is not among the finished, queued or pending scans"
+            unkObjects.append(h)
         else:
+    #ToDo:  drop the [pos/neg]Hash hashes in favor of the [pos/neg]Objects hashes
             print ('%d of %d scans identified this as malicious' % (json_response['positives'], json_response['total']))
             if json_response['positives']:
                 posHash[h]=json_response['positives']
+                posObjects[h]=json_response
             else:
                 negHash[h]=0
+                negObjects[h]=json_response
     else:
             print ('>%s< is not a valid MD5 hash' % h)
 #ToDo:  print summary:  posHash, negHash
-print ('done')
-
+print ('Lookups complete.  Summary:')
+print ('Items found to be malicious: %d' % len(posObjects))
+print ('Items found to be benign: %d' % len(negObjects))
+print ('No records found for %d items' % len(unkObjects))
 # Access individual elements with: json_response['sha1']
 
